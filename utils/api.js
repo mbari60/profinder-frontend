cc62c202b9973a3de1f64a7689ea7fd37b2c0954
mbari60 <kevinmbari60@gmail.com>
2025-04-04 22:22:30 +0300

    deploy

--- utils/api.js
+++ utils/api.js
@@ -1,9 +1,15 @@
-// lib/api.js
 import axios from "axios";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8002";
 
 // Create API instance with configuration
+// const api = axios.create({
+//   baseURL: API_URL,
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
+
 const api = axios.create({
   baseURL: API_URL,
   headers: {
